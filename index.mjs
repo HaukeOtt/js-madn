d417f79ce645f639feea625183a03d4b42ecc474
Hauke <hauke.otterstaetter@gmail.com>
2023-02-26 22:58:36 +0100

    the gamecan be tested with a list of actions

--- index.mjs
+++ index.mjs
@@ -1,5 +1,33 @@
 import * as game from "./madnGame.mjs"
 
-let gameObject = game.createGameObject(3);
+import * as fs from 'fs';
+
+let rawdata = fs.readFileSync('test0.json');
+let test0 = JSON.parse(rawdata);
+
+let rawdata1 = fs.readFileSync('dicePettern0.json');
+let dicetemplate = JSON.parse(rawdata1);
+
+
+let gameObject = game.createGameObject(2,dicetemplate);
 console.log(gameObject);
 console.log(gameObject.players[0]);
+
+
+
+for (let i = 0; i < test0.length; i++) {
+    const action = test0[i];
+    console.log();
+    console.log(`\x1b[34m${`do aktion nr.${i}:`}\x1b[0m`);
+    console.log(action);
+    let result = game.handleAction(gameObject,action);
+
+    
+    if(result.ok == false){
+        console.log(`\x1b[31m${"unvalid action:"}\x1b[0m`);
+        console.log(result);
+        console.log(gameObject);
+    }
+}
+
+console.log(gameObject);
--- madnGame.mjs
+++ madnGame.mjs
@@ -8,7 +8,7 @@ export function createGameObject(numOfPlayers) {
         
         inputState:
             1   weiting for dice clicked 
-    
+            2
             */
     //setting porpertys
     let numOfPlayslots = 4
@@ -68,29 +68,43 @@ function getPlayerIndexByNum(players, num) {
 }
 
 export function handleAction(gameObject, action) {
-
-    try {
-        switch (action.type) {
-            case 1:
-                handleInput1(gameObject);
-                break;
-            default:
-                return { ok: false, msg: "wrong action type" }
-                break;
-        }
-    } catch (error) {
-        return { ok: false, msg: "invalide action", err: error }
+    let result = { ok: true };
+    switch (action.type) {
+        case 1:
+            result = handleInput1(gameObject, action);
+            break;
+        case 2:
+            result = handleInput2(gameObject, action);
+            break;
+        default:
+            result = { ok: false, msg: "action type dose not exist" };
+            break;
     }
+
+    return result;
 
 }
 
-function handleInput1(gameObject) {
+function handleInput1(gameObject, action) {
+
+    if (action.type != gameObject.inputState) {
+        return { ok: false, msg: "wrong action type" }
+    }
+
+    if (action.playerNum != gameObject.playerInLine) {
+        return { ok: false, msg: "wrong player" }
+    }
+
+    //deactivate inputs while working
+    gameObject.inputState = 0;
+
     //find player with the lowest num, that has not roled the dice yet.
     let lowestPlayerNumWithoutDiceValue = null;
     for (let i = 0; i < gameObject.players.length; i++) {
         let player = gameObject.players[i]
         if (player.num !== null && player.lastDiceValue === null) {
             lowestPlayerNumWithoutDiceValue = player.num;
+            break;
         }
     }
 
@@ -101,9 +115,14 @@ function handleInput1(gameObject) {
         gameObject.players[
             getPlayerIndexByNum(gameObject.players, lowestPlayerNumWithoutDiceValue)
         ].lastDiceValue = gameObject.temp.dicevelue;
-        return;
+
+        //activate inputs bacause working is done
+        gameObject.inputState = 1;
+
+        return { ok: true };
     }
 
+    //get player with the highest lastDiceValue
     let playerNumWithHighestDiceValue = null;
     let highestDiceValue = null;
     for (let i = 0; i < gameObject.players.length; i++) {
@@ -118,10 +137,21 @@ function handleInput1(gameObject) {
             }
         }
     }
+    //role the dice for player in line
+    gameObject.temp.dicevelue = roleDice(gameObject.dice);
+    gameObject.players[
+        getPlayerIndexByNum(gameObject.players, playerNumWithHighestDiceValue)
+    ].lastDiceValue = gameObject.temp.dicevelue;
 
+    //activate inputs bacause working is done
+    gameObject.inputState = 2;
+    gameObject.playerInLine = playerNumWithHighestDiceValue;
 
-
-
+    return { ok: true };
 
 };
 
+function handleInput2(gameObject, action) {
+
+    return { ok: true };
+}
