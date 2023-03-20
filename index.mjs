40324e415706b18d3660726144dd21cf3c254b7c
Hauke <hauke.otterstaetter@gmail.com>
2023-03-20 16:20:50 +0100

    logs gameobject before crashing

--- index.mjs
+++ index.mjs
@@ -15,12 +15,21 @@ console.log(gameObject.players[0]);
 
 
 for (let i = 0; i < test0.length; i++) {
+    try {
+        
+    } catch (error) {
+        console.log(gameObject);
+        throw error;
+    }
     const action = test0[i];
     console.log();
     console.log(`\x1b[34m${`do aktion nr.${i}:`}\x1b[0m`);
     console.log(action);
     let result = game.handleAction(gameObject,action);
-
+console.table(gameObject.players[0]);
+console.table(gameObject.players[1]);
+console.table(gameObject.players[2]);
+console.table(gameObject.players[3]);
     
     if(result.ok == false){
         console.log(`\x1b[31m${"unvalid action:"}\x1b[0m`);
