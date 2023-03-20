172024425ee4951873f1d6706becbdde40528de6
Hauke <hauke.otterstaetter@gmail.com>
2023-03-20 17:08:45 +0100

    better debug log

--- index.mjs
+++ index.mjs
@@ -7,8 +7,9 @@ let test0 = JSON.parse(rawdata);
 let rawdata1 = fs.readFileSync('dicePettern0.json');
 let dicetemplate = JSON.parse(rawdata1);
 
+const PLAYER = 2
 
-let gameObject = game.createGameObject(2,dicetemplate);
+let gameObject = game.createGameObject(PLAYER, dicetemplate);
 console.log(gameObject);
 console.log(gameObject.players[0]);
 
@@ -16,7 +17,7 @@ console.log(gameObject.players[0]);
 
 for (let i = 0; i < test0.length; i++) {
     try {
-        
+
     } catch (error) {
         console.log(gameObject);
         throw error;
@@ -25,19 +26,28 @@ for (let i = 0; i < test0.length; i++) {
     console.log();
     console.log(`\x1b[34m${`do aktion nr.${i}:`}\x1b[0m`);
     console.log(action);
-    let result = game.handleAction(gameObject,action);
-console.table(gameObject.players[0]);
-console.table(gameObject.players[1]);
-console.table(gameObject.players[2]);
-console.table(gameObject.players[3]);
-    
-    if(result.ok == false){
+    let result = game.handleAction(gameObject, action);
+
+    let playersinfo = []
+    for (let i = 0; i < PLAYER; i++) {
+        gameObject.players.forEach(player => {
+            if (player.num == i) {
+                playersinfo.push(player.pawns);
+            }
+        });
+    }
+    console.table(playersinfo);
+    console.log({ playerInLine: gameObject.playerInLine,temp: gameObject.temp })
+
+    if (result.ok == false) {
         console.log(`\x1b[31m${"unvalid action:"}\x1b[0m`);
         console.log(result);
         console.log(gameObject);
-    }else
-    console.log(`result:`);
+    } else
+        console.log(`\x1b[32m${"result:"}\x1b[0m`);
     console.log(result)
 }
-
+console.log();
+console.log();
+console.log(`\x1b[32m${"end:"}\x1b[0m`)
 console.log(gameObject);
